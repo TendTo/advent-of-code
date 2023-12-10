c95464ab92c1ff0e60690f8bf6427a08d938679a
Ernesto Casablanca <casablancaernesto@gmail.com>
2023-12-10 18:36:26 +0000

    feat: day 10

--- 2023/src/test.py
+++ 2023/src/test.py
@@ -11,6 +11,7 @@ from c6 import Challenge6
 from c7 import Challenge7
 from c8 import Challenge8
 from c9 import Challenge9
+from c10 import Challenge10
 
 
 def mock_input(input_str: str = ""):
@@ -327,6 +328,85 @@ def test_challenge_9_part_1():
 )
 def test_challenge_9_part_2():
     assert Challenge9().second() == 2
+
+
+@use_input_fixture(
+    input_str="""
+-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF
+"""
+)
+def test_challenge_10_part_1():
+    assert Challenge10().first() == 4
+
+
+@use_input_fixture(
+    input_str="""
+7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ
+"""
+)
+def test_challenge_10_part_1_complex():
+    assert Challenge10().first() == 8
+
+
+@use_input_fixture(
+    input_str="""
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+"""
+)
+def test_challenge_10_part_2():
+    assert Challenge10().second() == 4
+
+
+@use_input_fixture(
+    input_str="""
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+"""
+)
+def test_challenge_10_part_2_complex():
+    assert Challenge10().second() == 8
+
+
+@use_input_fixture(
+    input_str="""
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+"""
+)
+def test_challenge_10_part_2_complex_2():
+    assert Challenge10().second() == 10
 
 
 if __name__ == "__main__":
